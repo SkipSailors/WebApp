2964ab87b60add2821e14173f0dc6559a1f3533c
Skip Sailors <ssailors@wpc-edi.com>
2023-02-06 07:04:15 -0800

    listing 18-9

---
+++ Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    /// <inheritdoc />
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Supplier> Suppliers => Set<Supplier>();
+}
