78d366a807a52a08f5ad2e43f8ce4dd3c3c161ef
Skip Sailors <ssailors@wpc-edi.com>
2023-02-11 17:54:53 -0800

    Listing 19-7

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,29 @@
-﻿namespace WebApp.Controllers
+﻿namespace WebApp.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
 {
-    public class ProductController
+    private DataContext context;
+
+    /// <inheritdoc />
+    public ProductController(DataContext ctx)
     {
+        context = ctx;
+    }
+
+    [HttpGet]
+    public IEnumerable<Product> GetProducts()
+    {
+        return context.Products;
+    }
+
+    [HttpGet("{id}")]
+    public Product? GetProduct([FromServices] ILogger<ProductController> logger)
+    {
+        logger.LogDebug("GetProduct Action Invoked");
+        return context.Products.FirstOrDefault();
     }
 }
