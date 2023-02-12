529e6df32730554622e1f48a88dcea025393ad5a
Skip Sailors <ssailors@wpc-edi.com>
2023-02-12 10:42:06 -0800

    listing 20-10

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApp.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ContentController : ControllerBase
+{
+    private DataContext context;
+
+    public ContentController(DataContext ctx)
+    {
+        context = ctx;
+    }
+
+    [HttpGet("string")]
+    public string GetString() => "This is a string response";
+
+    [HttpGet("object")]
+    public async Task<Product> GetObject()
+    {
+        return await context.Products.FirstAsync();
+    }
+}
