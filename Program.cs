c57460183188c24e91aa039d8f5adde5b7cccd02
Skip Sailors <ssailors@wpc-edi.com>
2023-02-12 13:56:32 -0800

    lsting 21-1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
 using WebApp.Models;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -10,28 +7,10 @@ builder.Services.AddDbContext<DataContext>(opts =>
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
     opts.EnableSensitiveDataLogging();
 });
-builder.Services.AddControllers().AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
-builder.Services.Configure<MvcNewtonsoftJsonOptions>(opts =>
-{
-    opts.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-});
-builder.Services.Configure<MvcOptions>(opts =>
-{
-    opts.RespectBrowserAcceptHeader = true;
-    opts.ReturnHttpNotAcceptable = true;
-});
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo(){Title = nameof(WebApp), Version = "v1"});
-});
+builder.Services.AddControllers();
 WebApplication app = builder.Build();
+app.UseStaticFiles();
 app.MapControllers();
-app.MapGet("/", () => "Hello World!");
-app.UseSwagger();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", nameof(WebApp));
-});
 DataContext context = app
     .Services
     .CreateScope()
