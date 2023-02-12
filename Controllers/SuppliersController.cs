0982e3dbebd240c16cc3213f5db5adbf38ef9daf
Skip Sailors <ssailors@wpc-edi.com>
2023-02-12 09:16:19 -0800

    listin 20-4

---
+++ Controllers/SuppliersController.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuppliersController: ControllerBase
+    {
+        private DataContext context;
+
+        public SuppliersController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Supplier?> GetSupplier(long id)
+        {
+            return await context
+                .Suppliers
+                .Include(s => s.Products)
+                .FirstAsync(s => s.SupplierId == id);
+        }
+    }
+}
