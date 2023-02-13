d9849b59cf6e147f0f226e7996cd75c211870bdb
Skip Sailors <ssailors@wpc-edi.com>
2023-02-12 17:34:21 -0800

    listing 21-14

---
+++ Controllers/SecondController.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class SecondController : Controller
+{
+    public IActionResult Index()
+    {
+        return View("Common");
+    }
+}
