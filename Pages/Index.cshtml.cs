51ac7832643982d232d2305d1ccd73eda16e3505
Skip Sailors <ssailors@wpc-edi.com>
2023-02-18 11:51:57 -0800

    listing 23-12

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp.Pages;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+
+public class IndexModel : PageModel
+{
+    private DataContext context;
+    public Product? Product { get; set; }
+
+    public IndexModel(DataContext ctx)
+    {
+        context = ctx;
+    }
+
+    public async Task OnGetAsync(long id = 1)
+    {
+        Product = await context.Products.FindAsync(id);
+    }
+}
