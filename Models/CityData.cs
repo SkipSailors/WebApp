2ee7f47bf20cf03145c29708c69e7503f74e39b5
Skip Sailors <ssailors@wpc-edi.com>
2023-02-19 11:49:55 -0800

    listing 24-2

---
+++ Models/CityData.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models;
+
+public class CityData
+{
+    private List<City> cities = new()
+    {
+        new City { Name = "London", Country = "UK", Population = 8539000 }
+    };
+}
