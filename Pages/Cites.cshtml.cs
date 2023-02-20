17f8609fa45fc4c75b11300c4ff15e272ec5d29d
Skip Sailors <ssailors@wpc-edi.com>
2023-02-19 19:54:10 -0800

    listing 24-30

--- Pages/Cites.cshtml.cs
+++ Pages/Cites.cshtml.cs
@@ -1,6 +1,34 @@
-﻿namespace WebApp.Pages
+﻿namespace WebApp.Pages;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Models;
+
+[ViewComponent(Name = "CitiesPageHybrid")]
+public class CitiesModel : PageModel
 {
-    public class Cites
+    public CitiesModel(CityData cdata)
     {
+        Data = cdata;
+    }
+
+    public CityData? Data { get; set; }
+
+    [ViewComponentContext] public ViewComponentContext Context { get; set; } = new();
+
+    public IViewComponentResult Invoke()
+    {
+        return new ViewViewComponentResult
+        {
+            ViewData = new ViewDataDictionary<CityViewModel>(
+                Context.ViewData,
+                new CityViewModel
+                {
+                    Cities = Data?.Cities.Count(),
+                    Population = Data?.Cities.Sum(c => c.Population)
+                })
+        };
     }
 }
