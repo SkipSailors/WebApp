2b5b11dd1a6b971acbb09c4c21f709f7bb4967d4
Skip Sailors <ssailors@wpc-edi.com>
2023-02-19 17:39:44 -0800

    listing 24-7

---
+++ Components/CitySummary.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp.Components;
+
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class CitySummary : ViewComponent
+{
+    private readonly CityData data;
+
+    public CitySummary(CityData cdata)
+    {
+        data = cdata;
+    }
+
+    public string Invoke()
+    {
+        return $"{data.Cities.Count()} cities, " +
+               $"{data.Cities.Sum(c => c.Population)}";
+    }
+}
