43405e37a66cba1baaf19db29f736dc6c1e8d13a
Skip Sailors <ssailors@wpc-edi.com>
2023-05-29 12:44:00 -0700

    listing 25-17

---
+++ TagHelpers/HighlightTagHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.TagHelpers;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+[HtmlTargetElement("*", Attributes = "[highlight=true")]
+public class HighlightTagHelper : TagHelper
+{
+    /// <inheritdoc />
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.PreContent.SetHtmlContent("<b><i>");
+        output.PostContent.SetHtmlContent("</i></b>");
+    }
+}
