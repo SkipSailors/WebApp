be5407d2e1ff7e3a2aa4fd070db8628c0ac04e0e
Skip Sailors <ssailors@wpc-edi.com>
2023-05-29 11:12:22 -0700

    listing 25-6

---
+++ TagHelpers/TrTagHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.TagHelpers;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+public class TrTagHelper : TagHelper
+{
+    public string BgColor { get; set; }
+    public string TextColor { get; set; }
+
+    /// <inheritdoc />
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.SetAttribute("class", $"bg-{BgColor} text-center text-{TextColor}");
+    }
+}
