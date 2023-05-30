d1cc95a521d0d0ca836264e249f88c88527c2634
Skip Sailors <ssailors@wpc-edi.com>
2023-05-29 18:41:07 -0700

    listing 25-30

---
+++ TagHelpers/TimeTagHelperComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApp.TagHelpers
+{
+    using System.Globalization;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    public class TimeTagHelperComponent:TagHelperComponent
+    {
+        /// <inheritdoc />
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string timestamp = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            if (output.TagName == "body")
+            {
+                TagBuilder elem = new("div");
+                elem.Attributes.Add("class", "bg-info text-white m-2 p-2");
+                elem.InnerHtml.Append($"Time:{timestamp}");
+                output.PreContent.AppendHtml(elem);
+            }
+        }
+    }
+}
